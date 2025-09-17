f46ca6a70db52e8891fd530fb03f3bfa503e42d6
반준 <banjoon87@gmail.com>
2025-09-17 15:19:24 +0900

    첫 커밋

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <div className="bg-blue-200 p-4 rounded-xl">Tailwind CSS</div>
+        <div className="bg-blue-200 p-4 rounded-r-xl">Tailwind CSS</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
