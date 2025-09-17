095f73f70f0e69fb403c5d0c50e38b9c986fa684
반준 <banjoon87@gmail.com>
2025-09-17 20:43:40 +0900

    홈, 네비 완성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 function Home() {
   return (
-    <div className="pt-24">
-      <h1> Home </h1>
-      <p>여기에 목록이 표시됩니다.</p>
+    <div className="pt-20">
+      {/* 메인 이미지 */}
+      <img src="imgs/202301_visual3.jpg" alt="홈 사진" className="" />
+
+      {/* 문구 영역 */}
+      <div className="mt-8 space-y-2 text-center">
+        <h1 className="text-3xl md:text-5xl font-extrabold text-yellow-400">
+          케이핏 소개
+        </h1>
+        <p className="text-xl md:text-2xl text-black pt-8 font-bold">
+          100% 사전예약제
+        </p>
+        <p className="text-xl md:text-2xl text-black pt-4 font-bold">
+          재활 트레이닝 전문
+        </p>
+        <p className="text-xl md:text-2xl text-black pt-4 font-bold">
+          1:1 PT & 1:1 PILATES
+        </p>
+        <p className="text-xl md:text-2xl text-black pt-4 font-bold">
+          "건강한 변화의 시작 케이핏"
+        </p>
+      </div>
     </div>
   );
 }
