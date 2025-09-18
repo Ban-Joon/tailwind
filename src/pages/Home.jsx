4958002a673501bbbbc20d166a53b2b797917750
반준 <banjoon87@gmail.com>
2025-09-18 17:34:45 +0900

    모바일 사진 자리

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,24 @@
 function Home() {
   return (
-    <div className="pt-26 md:pt-20">
+    <div className="pt-84 md:pt-20">
       {/* 메인 이미지 */}
       <img src="imgs/202301_visual3.jpg" alt="홈 사진" className="" />
 
       {/* 문구 영역 */}
       <div className="mt-8 space-y-2 text-center">
-        <h1 className="text-3xl md:text-5xl font-extrabold text-yellow-400">
+        <h1 className="text-2xl md:text-5xl font-extrabold text-yellow-400">
           케이핏 소개
         </h1>
-        <p className="text-xl md:text-2xl text-gray-700 pt-8 font-bold">
+        <p className="text-md md:text-2xl text-gray-700 pt-8 font-bold">
           100% 사전예약제
         </p>
-        <p className="text-xl md:text-2xl text-gray-700 pt-4 font-bold">
+        <p className="text-md md:text-2xl text-gray-700 pt-4 font-bold">
           재활 트레이닝 전문
         </p>
-        <p className="text-xl md:text-2xl text-gray-700 pt-4 font-bold">
+        <p className="text-md md:text-2xl text-gray-700 pt-4 font-bold">
           1:1 PT & 1:1 PILATES
         </p>
-        <p className="text-xl md:text-2xl text-gray-700 pt-4 font-bold">
+        <p className="text-md md:text-2xl text-gray-700 pt-4 font-bold">
           "건강한 변화의 시작 케이핏"
         </p>
       </div>
